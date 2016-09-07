d5712076cbb0053a6e7297667d073587cfe38db2
Jesse Alas <jessealas94@gmail.com>
2016-09-07 11:30:59 -0700

    Removed old unecessary code.

--- sqlitepp/sql/expression_base.hxx
+++ sqlitepp/sql/expression_base.hxx
@@ -10,7 +10,9 @@ namespace sqlitepp {
         struct expression_base {
             constexpr auto get_str() const { return static_cast<const T*>(this)->get_str(); }
             constexpr expression_type get_type() const { return static_cast<const T*>(this)->get_type(); }
-            constexpr operator T() const { return *static_cast<const T*>(this); }
+
+            constexpr operator const T&() const { return *static_cast<const T *>(this); }
+            constexpr operator T&() { return *static_cast<T *>(this); }
         };
 
     }

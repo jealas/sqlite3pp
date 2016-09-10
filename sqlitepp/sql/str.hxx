2485638f0154618bef2c5d42502d1e29370f5159
Jesse Alas <jessealas94@gmail.com>
2016-09-10 09:40:33 -0700

    Refactored SQL string class and user defined literal for it.

--- sqlitepp/sql/str.hxx
+++ sqlitepp/sql/str.hxx
@@ -3,18 +3,26 @@
 namespace sqlitepp {
     namespace sql {
 
+        class str;
+        constexpr str operator ""_sql_str(const char *, std::size_t);
+
         class str {
+            friend constexpr str operator ""_sql_str(const char *, std::size_t);
+
         public:
-            constexpr str(const char *c_str, std::size_t size) : c_str{c_str}, size{size} {}
+            constexpr const char * c_str() const { return c_str_ptr; }
+            constexpr std::size_t length() const { return size; }
 
         private:
-            const char * c_str;
+            constexpr str(const char *c_str_ptr, std::size_t size) : c_str_ptr{c_str_ptr}, size{size} {}
+
+        private:
+            const char * c_str_ptr;
             const std::size_t size;
         };
 
+        constexpr str operator ""_sql_str(const char * str, std::size_t size) {
+            return {str, size};
+        }
     }
-}
-
-constexpr sqlitepp::sql::str operator "" _sql_str(const char * str, std::size_t size) {
-    return {str, size};
 }

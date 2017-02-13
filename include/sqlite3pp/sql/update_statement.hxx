1fca3a6cf5946cb216b171a1d7331fc9dfa36657
jealas <jessealas94@gmail.com>
2017-02-12 22:35:35 -0800

    Implemented UPDATE OR with table syntax.

--- include/sqlite3pp/sql/update_statement.hxx
+++ include/sqlite3pp/sql/update_statement.hxx
@@ -1,7 +1,8 @@
 #pragma once
 
 
-#include "sql_strings.hxx"
+#include "sqlite3pp/sql/table.hxx"
+#include "sqlite3pp/sql/sql_strings.hxx"
 
 namespace sqlite3pp {
     namespace sql {
@@ -16,61 +17,99 @@ namespace sqlite3pp {
             constexpr auto to_str() const { return static_cast<const UpdateSyntaxT *>(this)->to_str(); }
         };
 
+        template <class UpdateSyntaxT, class TableT>
+        class update_with_table : public update_syntax<update_with_table<UpdateSyntaxT, TableT>> {
+        public:
+            constexpr update_with_table(const update_syntax<UpdateSyntaxT> &update_syntax, const table_base<TableT> &table)
+                : update_syntax{update_syntax}, table{table} {}
+
+            constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), table.get_name()); }
+
+        private:
+            const update_syntax<UpdateSyntaxT> &update_syntax;
+            const table_base<TableT> &table;
+        };
+
         template <class UpdateSyntaxT>
-        class update_rollback_syntax {
+        class update_rollback_syntax : public update_syntax<update_rollback_syntax<UpdateSyntaxT>> {
         public:
             constexpr update_rollback_syntax(const update_syntax<UpdateSyntaxT> &update_syntax)
                 : update_syntax{update_syntax} {}
 
             constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), sql_strings::ROLLBACK); }
 
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_rollback_syntax<UpdateSyntaxT>, TableT>{*this, table};
+            }
+
         private:
             const update_syntax<UpdateSyntaxT> &update_syntax;
         };
 
         template <class UpdateSyntaxT>
-        class update_abort_syntax {
+        class update_abort_syntax : public update_syntax<update_abort_syntax<UpdateSyntaxT>> {
         public:
             constexpr update_abort_syntax(const update_syntax<UpdateSyntaxT> &update_syntax)
                     : update_syntax{update_syntax} {}
 
             constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), sql_strings::ABORT); }
 
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_abort_syntax<UpdateSyntaxT>, TableT>{*this, table};
+            }
+
         private:
             const update_syntax<UpdateSyntaxT> &update_syntax;
         };
 
         template <class UpdateSyntaxT>
-        class update_replace_syntax {
+        class update_replace_syntax : public update_syntax<update_replace_syntax<UpdateSyntaxT>> {
         public:
             constexpr update_replace_syntax(const update_syntax<UpdateSyntaxT> &update_syntax)
                     : update_syntax{update_syntax} {}
 
             constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), sql_strings::REPLACE); }
 
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_replace_syntax<UpdateSyntaxT>, TableT>{*this, table};
+            }
+
         private:
             const update_syntax<UpdateSyntaxT> &update_syntax;
         };
 
         template <class UpdateSyntaxT>
-        class update_fail_syntax {
+        class update_fail_syntax : public update_syntax<update_fail_syntax<UpdateSyntaxT>> {
         public:
             constexpr update_fail_syntax(const update_syntax<UpdateSyntaxT> &update_syntax)
                     : update_syntax{update_syntax} {}
 
             constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), sql_strings::FAIL); }
 
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_fail_syntax<UpdateSyntaxT>, TableT>{*this, table};
+            }
+
         private:
             const update_syntax<UpdateSyntaxT> &update_syntax;
         };
 
         template <class UpdateSyntaxT>
-        class update_ignore_syntax {
+        class update_ignore_syntax : public update_syntax<update_ignore_syntax<UpdateSyntaxT>> {
         public:
             constexpr update_ignore_syntax(const update_syntax<UpdateSyntaxT> &update_syntax)
                     : update_syntax{update_syntax} {}
 
             constexpr auto to_str() const { return sql_strings::SPACE.join(update_syntax.to_str(), sql_strings::IGNORE); }
+
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_ignore_syntax<UpdateSyntaxT>, TableT>{*this, table};
+            }
 
         private:
             const update_syntax<UpdateSyntaxT> &update_syntax;
@@ -87,6 +126,7 @@ namespace sqlite3pp {
                   FAIL{*this},
                   IGNORE{*this} {}
 
+        public:
             update_rollback_syntax<update_or_member<UpdateSyntaxT>> ROLLBACK;
             update_abort_syntax<update_or_member<UpdateSyntaxT>> ABORT;
             update_replace_syntax<update_or_member<UpdateSyntaxT>> REPLACE;
@@ -105,8 +145,13 @@ namespace sqlite3pp {
 
             constexpr auto to_str() const { return sql_strings::UPDATE; }
 
-            update_or_member<update_member> OR;
+            template <class TableT>
+            constexpr auto operator()(const table_base<TableT> &table) const {
+                return update_with_table<update_member, TableT>{*this, table};
+            }
 
+        public:
+            update_or_member<update_member> OR;
         };
 
     }
--- tests/test_update_statement.cxx
+++ tests/test_update_statement.cxx
@@ -2,10 +2,25 @@
 
 #include <algorithm>
 
+// Catch defines a FAIL macro which interferes with the SQL FAIL syntax below.
+// FAIL will be undefined for this file as it is unused.
+#undef FAIL
+
 #include "sqlite3pp/query.hxx"
 
 namespace {
     using namespace sqlite3pp;
+
+    template <class Name, class Age>
+    struct person_table {
+        Name name;
+        Age age;
+    };
+
+    static constexpr auto age = column("age");
+    static constexpr auto name = column("name");
+
+    constexpr auto person = table<person_table>("person", name, age);
 }
 
 
@@ -49,4 +64,42 @@ TEST_CASE("UPDATE OR IGNORE syntax works", "[test-update-statement]") {
     constexpr auto update_str = UPDATE.OR.IGNORE.to_str();
 
     REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR IGNORE"));
+}
+
+TEST_CASE("UPDATE table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE person"));
+}
+
+//
+
+TEST_CASE("UPDATE OR ROLLBACK table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE.OR.ROLLBACK(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR ROLLBACK person"));
+}
+
+TEST_CASE("UPDATE OR ABORT table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE.OR.ABORT(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR ABORT person"));
+}
+
+TEST_CASE("UPDATE OR REPLACE table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE.OR.REPLACE(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR REPLACE person"));
+}
+
+TEST_CASE("UPDATE OR FAIL table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE.OR.FAIL(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR FAIL person"));
+}
+
+TEST_CASE("UPDATE OR IGNORE table syntax works", "[test-update-statement]") {
+    constexpr auto update_str = UPDATE.OR.IGNORE(person).to_str();
+
+    REQUIRE(std::equal(update_str.begin(), update_str.end(), "UPDATE OR IGNORE person"));
 }
